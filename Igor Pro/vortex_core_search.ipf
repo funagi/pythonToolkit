4263d72b9d0f808bf027527388a4ace616a94111
KeMeGe@KeMeGe-PC <KeMeGe@KeMeGe-PC>
2012-05-26 15:24:52 +0800

    vortex_core_search:去掉了在读取文件与拟合时的弹出窗口，能够静默运行                    添加了进度条显示                    途中可以取消 ParseMovie:制作了基于Tkinter的GUI            可以自动搜索文件夹下的文件并提取Basename            可以跳过已存在的文件            能够弹窗提示错误

--- Igor Pro/vortex_core_search.ipf
+++ Igor Pro/vortex_core_search.ipf
@@ -1,36 +1,8 @@
 #pragma rtGlobals=1		// Use modern global access method.
 Menu "&OOMMF"
 	"Find vortex cores...", OpenOMFBatch2()
-	//"Import Vortex Field File...", ParseOMF()
 End
 //将vortex trilayer nanodisk dynamics 的omf文件进行批处理：将mz分量map成为二维图像，通过拟合得到vortex core position, 
-// old : basename需要手工输入
-Function OpenOMFBatch()
-	Variable refNum
-	String S_path,S_filename
-	Open /D /R /F="OOMMF Field Files (*.o?f):.o?f;" /M="Choose an OOMMF File" refNum
-	If( CmpStr(S_filename,"")==0 )
-		return 0
-	Endif
-	String /G Path=ParseFilePath(1,S_Filename,":",1,0)
-	
-	String basename="rotating-Oxs_TimeDriver-Magnetization-00-", filename
-	Variable step=20, maxvalue=100,minvalue=0,i,count
-	Prompt basename, "Input basename:"
-	Prompt step, "Input step size:"
-	Prompt minvalue, "Input min value:"
-	Prompt maxvalue, "Input max value:"
-	DoPrompt "Enter parameters", basename,step,minvalue,maxvalue
-	count=(maxvalue-minvalue)/step
-	Make/N=(count,5)/D/O datatable
-	NewPath Data path
-	For (i=minvalue;i<maxvalue;i+=step)
-		sprintf filename, "%s%07d.omf", basename, i
-		parseData(filename, (i-minvalue)/step, datatable)
-	Endfor
-End
-
-// new : 自动提取basename
 Function OpenOMFBatch2()
 	Variable refNum
 	String S_path,S_filename
@@ -43,28 +15,45 @@ Function OpenOMFBatch2()
 	
 	String basename=Filename0[0,strlen(Filename0)-12], filename
 	Variable step=20, maxvalue=100,minvalue=0,i,count
+	Variable z=3
 	Prompt step, "Input step size:"
 	Prompt minvalue, "Input min value:"
 	Prompt maxvalue, "Input max value:"
-	DoPrompt "Enter parameters", step,minvalue,maxvalue
+	Prompt z, "Input Z-Slice:"
+	DoPrompt "Enter parameters", step,minvalue,maxvalue,z
 	count=(maxvalue-minvalue)/step
 	Make/N=(count,5)/D/O datatable
 	NewPath Data Path
+	
+	//显示进度条窗口
+	NewPanel /N=Progress /W=(285,111,739,193)
+	ValDisplay valdisp0, pos={18,32}, size={342,18}
+	ValDisplay valdisp0, limits={0,maxvalue,0},barmisc={0,0}
+	ValDisplay valdisp0, mode=3, frame=0
+	ValDisplay valdisp0, value=_NUM:0
+	Button bStop, pos={375,32}, size={50,20}, title="Stop"
+	DoUpdate /W=Progress /E=1
 	For (i=minvalue;i<maxvalue;i+=step)
-		//printf "%s%07d.omf", basename, i
 		sprintf filename, "%s%07d.omf", basename, i
-		parseData(filename, (i-minvalue)/step, datatable)
+		parseData(filename, (i-minvalue)/step, datatable, z)
+		//更新进度条的值
+		ValDisplay valdisp0, value=_NUM:i+1, win=Progress
+		DoUpdate /W=Progress
+		if (V_Flag == 2)
+			break
+		Endif
 	Endfor
+	KillWindow Progress
 End
 //对单个omf文件处理
-function parseData(filename, position, wname)
+function parseData(filename, position, wname, z)
 	String filename
-	Variable position
+	Variable position,z
 	wave wname
 	Variable timevalue
 	wave coef
 	//find time
-	Grep /E="Total simulation time" /LIST /P=Data filename
+	Grep /E="Total simulation time" /Q /LIST /P=Data filename
 	If(V_value==1)
 		sscanf S_value, "# Desc:  Total simulation time: %es;", timevalue
 	Else
@@ -72,12 +61,14 @@ function parseData(filename, position, wname)
 		return 0
 	Endif
 	//find peaks
-	LoadWave /A /B="C=1,N='_skip_';C=1,N='_skip_';C=1,N=mz1;" /D /G /O /W /P=Data filename
+	LoadWave /A /B="C=1,N='_skip_';C=1,N='_skip_';C=1,N=mz1;" /D /G /O /Q /W /P=Data filename
 	duplicate/O mz1 mz2
-	Redimension/N=(100,100,3) mz1,mz2
-	DeletePoints/M=2 1,2, mz1
-	DeletePoints/M=2 0,2, mz2
+	Redimension/N=(100,100,z) mz1,mz2
+	DeletePoints/M=2 1,z-1, mz1
+	DeletePoints/M=2 0,z-1, mz2
 	Redimension/N=(-1,-1) mz1,mz2
+	wavetransform/o abs mz1 //将矩阵去绝对值后在进行寻峰（峰值也有可能是最小值）
+	wavetransform/o abs mz2 
 	peak(mz1)
 	wname[position][1]=coef[2]
 	wname[position][2]=coef[4]
@@ -85,7 +76,7 @@ function parseData(filename, position, wname)
 	wname[position][3]=coef[2]
 	wname[position][4]=coef[4]
 	//write time value
-	wname[position][0]=timevalue
+	wname[position][0]=timevalue*1e9 //单位换算为ns
 End
 //找到mz并拟合寻峰
 function peak (mz)
@@ -102,6 +93,12 @@ for (i=0;i<100;i+=1)
 	endfor
 endfor
 // printf "peakposition x=%g, y=%g" x, y 
-CurveFit/NTHR=0 Gauss2D kwCWave=coef,  mz[x-10,x+10][y-10,y+10]/D
+CurveFit/NTHR=0 /Q /N /W=2 Gauss2D kwCWave=coef,  mz[x-10,x+10][y-10,y+10]/D
 end
 
+Function Example1()
+Variable i
+for(i=0;i<5;i+=1)
+print i
+endfor
+End
---
+++ OOMMF DataTable Parse/ParseMovieGUI.py
@@ -0,0 +1,193 @@
+﻿from Tkinter import *
+from ttk import *
+
+class Application(Frame):
+    def createWidgets(self):
+        self.columnconfigure(0, pad=3)
+        self.columnconfigure(1, pad=3, weight=1)
+        self.rowconfigure(0, pad=3)
+        self.rowconfigure(1, pad=3)
+        self.rowconfigure(2, pad=3)
+        self.rowconfigure(3, pad=3)
+        self.rowconfigure(4, pad=3)
+        self.rowconfigure(5, pad=3)
+        self.rowconfigure(6, pad=3)
+        self.rowconfigure(7, pad=3)
+        self.rowconfigure(8, pad=3)
+
+        self.rowconfigure(9, pad=3)
+        self.rowconfigure(10, pad=3)
+        self.rowconfigure(11, pad=3)
+        self.rowconfigure(12, pad=3)
+        
+        self.i=0
+        self.label = Label(self, text = 'Folder:')
+        self.label.grid(row=self.i, column=0, sticky=W)
+        self.edtFolder = Entry(self)
+        self.edtFolder.grid(row=self.i, column=1, sticky=W+E)
+        self.i+=1
+        
+        self.label = Label(self, text = 'Basename:')
+        self.label.grid(row=self.i, column=0, sticky=W)
+        self.edtBase = Entry(self)
+        self.edtBase.grid(row=self.i, column=1, sticky=W+E)
+        self.i+=1
+        
+        self.label = Label(self, text = 'Filename Stepsize:')
+        self.label.grid(row=self.i, column=0, sticky=W)
+        self.edtStep = Entry(self)
+        self.edtStep.grid(row=self.i, column=1, sticky=W+E)
+        self.edtStep.insert(0,'50')
+        self.i+=1
+        
+        self.label = Label(self, text = 'File Count:')
+        self.label.grid(row=self.i, column=0, sticky=W)
+        self.edtCount = Entry(self)
+        self.edtCount.grid(row=self.i, column=1, sticky=W+E)
+        self.i+=1
+        
+        self.label = Label(self, text = 'Left Text:')
+        self.label.grid(row=self.i, column=0, sticky=W)
+        self.edtLeft = Entry(self)
+        self.edtLeft.grid(row=self.i, column=1, sticky=W+E)
+        self.edtLeft.insert(0,'z=1 layer')
+        self.i+=1
+        
+        self.label = Label(self, text = 'Right Text:')
+        self.label.grid(row=self.i, column=0, sticky=W)
+        self.edtRight = Entry(self)
+        self.edtRight.grid(row=self.i, column=1, sticky=W+E)
+        self.edtRight.insert(0,'z=2 layer')
+        self.i+=1
+        
+        self.label = Label(self, text = 'OOMMF Script Path:')
+        self.label.grid(row=self.i, column=0, sticky=W)
+        self.edtOMF = Entry(self)
+        self.edtOMF.grid(row=self.i, column=1, sticky=W+E)
+        self.edtOMF.insert(0,'E:\\oommf12a4\\oommf.tcl')
+        self.i+=1
+        
+        self.label = Label(self, text = 'FFMpeg Path:')
+        self.label.grid(row=self.i, column=0, sticky=W)
+        self.edtFFMpeg = Entry(self)
+        self.edtFFMpeg.grid(row=self.i, column=1, sticky=W+E)
+        self.edtFFMpeg.insert(0,'E:\\ffmpeg.exe')
+        self.i+=1
+
+        
+        self.btnStart = Button(self)
+        self.btnStart['text'] = u'  Start  '
+        #self.btnStart['font'] = u'宋体 8'
+        self.btnStart['command'] = self.Start
+        self.btnStart.grid(row=self.i, column=0, columnspan=2)
+        self.i+=1
+
+        
+        
+        self.pack()
+        
+        
+    def __init__(self, master = None):
+        Frame.__init__(self, master)
+        self.pack(expand = YES, fill = BOTH)
+        self.master.title('Index Local Version')
+        self.createWidgets()
+        
+    def ParseFileName(dir,str1,str2,ext):
+        flist = os.listdir(dir)
+        for f in flist:
+            if not ext in f:
+                continue
+            try:
+                os.rename(dir+f,dir+f.replace(str1,str2))
+            except Exception as e:
+                print e
+                
+    def PromptError(self, msg):
+        print msg
+        
+    def Start(self):
+        #create progress widgets
+        self.label = Label(self, text = 'Progress:')
+        self.label.grid(row=self.i, column=0, sticky=W)
+        self.prog = Progressbar(self)
+        self.prog.grid(row=self.i, column=1, sticky=W+E)
+        self.pack()
+        #Start working!
+        
+        #get parameters
+        folder = self.edtFolder.get()
+        oommf = self.edtOMF.get()
+        ffmpeg = self.FFMpeg.get()
+        basename = self.edtBase.get()
+        factor = self.edtStep.get()
+        count = self.edtCount.get()
+        left = self.edtLeft.get()
+        right = self.edtRight.get()
+        minvalue = -200
+        maxvalue = 200
+        #see if parameters are right
+        if folder[len(folder)-1]=='\\':
+            folder = folder[:len(folder)-1]
+        
+        #code from ParseMovie.py
+        #Parse .omf files with oommf avf2ppm
+        os.system('pushd %s' % folder)
+        os.mkdir('%s\\Movie' % folder)
+        os.system('tclsh84 %s avf2ppm -config 1.config -format B24 -ipat *.omf' % oommf)
+        self.ParseFileName('%s\\'%folder,basename,'up','bmp')
+        os.system('move *.bmp Movie\\')
+
+        os.system('tclsh84 %s avf2ppm -config 2.config -format B24 -ipat *.omf' % oommf)
+        self.ParseFileName('%s\\'%folder,basename,'down','bmp')
+        os.system('move *.bmp Movie\\')
+
+        #Code from MergeImage_Spec.py
+        errors=[]
+        redpoint=[]
+        textfont = ImageFont.truetype('cambriai.ttf',30)
+        for i in range(0,count):
+            try:
+                img1 = Image.open('Movie\\up%05d.bmp'%(i*factor))
+                img2 = Image.open('Movie\\down%05d.bmp'%(i*factor))
+
+                width = img1.size[0] + img2.size[0]
+                height = img1.size[1] + 101# + img2.size[1]
+
+                newimg = Image.new('RGBA', (width, height),(255,255,255))
+                newimg.paste(img1,(0,0,img1.size[0],img1.size[1]))
+                newimg.paste(img2,(img1.size[0],0,img1.size[0]+img2.size[0],img2.size[1]))
+
+                draw = ImageDraw.Draw(newimg)
+                
+                textrect = draw.textsize(left,font = textfont)
+                draw.text((width/4-textrect[0]/2,height-100), left, fill = (0,0,0), font = textfont)
+                textrect = draw.textsize(right,font = textfont)
+                draw.text((width*3/4-textrect[0]/2,height-100), right, fill = (0,0,0), font = textfont)
+                draw.line((10,height-25,width-10,height-25), fill = (0,0,0))
+
+                rect_center = 15+(width-25)*i/count
+                draw.rectangle((rect_center-5,height-30,rect_center+5,height-20), fill = (0,0,0))
+
+                #draw keypoints
+                if i in redpoint:
+                    draw.rectangle((rect_center-2,568,rect_center+2,572), fill = (255,0,0))
+                
+                newimg.save('Movie\\Mov%03d.png'%(i))
+                print('Succeeded: '+'Mov%03d.png'%(i))
+            except Exception as e:
+                errors.append(i)
+                print e
+
+        if len(errors)!=0:
+            print 'Errors:',str(errors)
+
+        #Make mp4 file with ffmpeg
+        print '%s -r 15 -i Movie\\Mov%%03d.png out.mp4' % ffmpeg
+        os.system('%s -r 15 -i Movie\\Mov%%03d.png out.mp4' % ffmpeg)
+        
+        
+root = Tk()
+app = Application(master = root)
+app.mainloop()
+root.destroy()

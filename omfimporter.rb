a3813f895d016dbeaede979e1f7a5669e1463952
KeMeGe@KeMeGe-PC <KeMeGe@KeMeGe-PC>
2012-05-27 12:15:25 +0800

    OMFImporter:重写了读取磁化强度方向的代码             处理时考虑了不同方式生成的omf文件中磁化的数值大小的不同             改进了添加模型时的代码，速度大大加快（其实是原来太慢了）

--- omfimporter.rb
+++ omfimporter.rb
@@ -17,45 +17,73 @@ class OMFImporter < Sketchup::Importer
 	end
 
 	def load_file(file_path, status)
-		#UI.messagebox(file_path)
+
+		#draw basic information
 		file = File.new(file_path, "r")
 		content = file.read
-		#UI.messagebox(content)
 		begin
 			xmax = /(xnodes: )(\d+)/.match(content)[2]
 			ymax = /(ynodes: )(\d+)/.match(content)[2]
 			zmax = /(znodes: )(\d+)/.match(content)[2]
-			datalist = /(# Begin: Data Text)((\w|\W)+)(# End: Data Text)/.match(content)[2]
-			regex = /((-|)\d+.\d+(e(-|)\d+|))/
-			datatable = datalist.scan(regex)
-			arrow = Sketchup.active_model.definitions.load(
+			zslice = UI.inputbox(["Please input Z-Slice (0~"+(zmax.to_i-1).to_s+"):"])
+			znum = zslice[0].to_i
+			if znum==-1
+				zmin=0
+			else
+				zmin=znum
+				zmax=(znum+1).to_s
+			end
+		file.close
+
+		lines = IO.readlines(file_path)
+		offset = nil
+		maxvalue = nil
+		lines.each do |line|
+			if line.index("ValueRangeMaxMag")!=nil
+				maxvalue = /(# ValueRangeMaxMag: )(\d+(.\d+|))(\n)/.match(line)[2]
+			end
+			if line.index("Begin: Data Text")!=nil
+				offset = lines.index(line)+1
+				break
+			end
+		end
+		#draw sth.
+		arrow = Sketchup.active_model.definitions.load(
 				Sketchup.find_support_file("arrow.skp", "Components/Components Sampler/"))
-			for z in 0..zmax.to_i-1
-				for y in 0..ymax.to_i-1
-					for x in 0..xmax.to_i-1
-						offset = 3*(z*ymax.to_i*xmax.to_i+y*xmax.to_i+x)
-						px = datatable[offset][0].to_f
-						#UI.messagebox(datatable[offset][0])
-						py = datatable[offset+1][0].to_f
-						pz = datatable[offset+2][0].to_f
-						axis = Geom::Vector3d.new(
-							px/Math.sqrt(2*pz+2),
-							py/Math.sqrt(2*pz+2),
-							Math.sqrt(0.5*pz+0.5)
-							)
-						point = Geom::Point3d.new(x*60,y*60,z*60)
-						entities = Sketchup.active_model.entities
-						group = entities.add_group();groupEnts = group.entities
- 						#groupEnts.add_3d_text('X', TextAlignLeft, "Symbol",
- 						#	true, false, 1.0, 0.0, 0.5, true, 0.1)
-						groupEnts.add_instance(
-							arrow, 
-							Geom::Transformation.rotation([0,0,30],axis,3.14)
-							)
- 						group.move!(point)
+		entities = Sketchup.active_model.entities
+		group = entities.add_group();
+		for z in zmin..zmax.to_i-1
+			for y in 0..ymax.to_i-1
+				for x in 0..xmax.to_i-1
+					line = lines[offset+z*xmax.to_i*ymax.to_i+y*ymax.to_i+x]
+					datatable = line.scan(/((-|)\d+(\.\d+|)(e(-|)\d+|))/)
+					#UI.messagebox(lines[offset+z*xmax.to_i*ymax.to_i+y*ymax.to_i+x])
+					px = datatable[0][0].to_f/maxvalue.to_f
+					py = datatable[1][0].to_f/maxvalue.to_f
+					pz = datatable[2][0].to_f/maxvalue.to_f
+					
+					if px==0 && py==0 && pz==0
+						next
 					end
+					
+					axis = Geom::Vector3d.new(
+						px/Math.sqrt(2*pz+2),
+						py/Math.sqrt(2*pz+2),
+						Math.sqrt(0.5*pz+0.5)
+						)
+					#UI.messagebox("#{datatable[0][0]} #{datatable[1]} #{datatable[2]} \n #{axis}")
+					point = Geom::Point3d.new(x*60,y*60,z*60)
+					groupEnts = group.entities
+					inst = groupEnts.add_instance(
+						arrow, 
+						Geom::Transformation.rotation([0,0,27.4],axis,3.14)
+						)
+					inst.move!(point)
+					trans = inst.transform! Geom::Transformation.rotation([x*60,y*60,z*60+27.4],axis,3.14)
 				end
 			end
+		end
+
 		rescue => err
 			UI.messagebox(err)
 			UI.messagebox()

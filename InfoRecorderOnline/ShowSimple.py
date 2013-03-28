c9151571df2c2baba73ebfa1c0784c99fd839181
KeMeGe <none@none>
2013-03-28 20:31:09 +0800

    InfoRecorderOnline: 全文索引模块可以根据DataTables的请求进行搜索并返回结果，但DataTables前台未完成

--- InfoRecorderOnline/ShowSimple.py
+++ InfoRecorderOnline/ShowSimple.py
@@ -1,12 +1,150 @@
 #encoding:utf-8
 from google.appengine.ext import webapp
 from google.appengine.api import users
+from google.appengine.api import search
 from google.appengine.ext import db
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from google.appengine.ext.db import BadKeyError
 from basetypes import *
 import os,datetime,logging,json
+
+class ShowCenterComplete(webapp.RequestHandler):
+    def get(self):
+        is_ajax = 'ajax' in self.request.arguments()
+
+        stat_type = self.request.get('type','')
+        if stat_type == '':
+            template_values = {
+                'title' : '查看数据',
+                'ajax' : is_ajax,
+                'tablist' : [
+                    {'name' : '游戏', 'href' : '/show/complete?type=game'},
+                    {'name' : '角色', 'href' : '/show/complete?type=chara'},
+                    {'name' : '声优', 'href' : '/show/complete?type=seiyuu'},
+                    {'name' : '会社', 'href' : '/show/complete?type=comp'},
+                ],
+                'type' : 'show'
+            }
+            path = os.path.join(os.path.dirname(__file__), './/template//tab.html')
+            self.response.out.write(template.render(path,template_values))
+            return
+        else:
+            type_map = {
+                'game' : {
+                    'serve_url' : '/show/complete/game', 
+                    'columns' : '7', 
+                    'headers': ['图标', '名称', '会社', '发行日期', '开始日期', '结束日期', '编辑']
+                    },
+                'chara' : {
+                    'serve_url' : '/show/complete/chara', 
+                    'columns' : '5', 
+                    'headers': ['Image', 'CID', 'Name', 'Romaji', 'Seiyuu', 'SID', 'Action']
+                    },
+                'seiyuu' : {
+                    'serve_url' : '/show/complete/seiyuu', 
+                    'columns' : '4', 
+                    'headers': ['SID', 'Name', 'isMain', 'VADB Link']
+                    },
+                'comp' : {
+                    'serve_url' : '/show/complete/comp', 
+                    'columns' : '2', 
+                    'headers': ['序号', '名称']
+                    },
+                }
+
+            if stat_type not in type_map.keys():
+                self.error(404)
+                return
+
+            template_values = type_map[stat_type]
+
+            path = os.path.join(os.path.dirname(__file__), './/template//show.html')
+            self.response.out.write(template.render(path,template_values))
+
+class ShowCharactersComplete(webapp.RequestHandler):
+    def get(self):
+        charas = Character.all()
+
+        charas.order('cid')
+
+        datalist = []
+        for chara in charas:
+            datalist.append([
+                '<img src="/data?key=%s" width=50 height=50/>' % chara.image.key(),
+                chara.cid,
+                chara.Name,
+                chara.Romaji,
+                chara.getSeiyuuName(),
+                chara.sid,
+                '<a href="/edit/charadetail?key=%s">Edit</a>' % chara.key(),
+                ])
+
+        #----end------------------------------------------------------------------------------------------
+        jsondata = {
+            'aaData' : datalist
+        }
+        self.response.out.write(json.dumps(jsondata))
+
+class ShowSeiyuuComplete(webapp.RequestHandler):
+    def get(self):
+        seiyuus = Seiyuu.all()
+        seiyuus.order('sid')
+        datalist = []
+        for item in seiyuus:
+            datalist.append([
+                item.sid,
+                item.Name,
+                str(item.isMain),
+                '<a href="http://beautyplanets.web.fc2.com/VADB/actor/c_%04d.html">%d</a>'%(item.snum,item.snum)
+                ])
+
+        jsondata = {
+            'aaData' : datalist
+        }
+
+        self.response.out.write(json.dumps(jsondata))
+
+class ShowGameComplete(webapp.RequestHandler):
+    def get(self):
+        games = game.all()
+        games.order('rDate')
+        datalist = []
+        for item in games:
+            datalist.append([
+                '<img src="/data?key=%s" width=50 height=50/>'%item.Icon.key(),
+                item.Name,
+                db.get(item.Company).Name,
+                item.rDate.strftime('%Y-%m-%d'),
+                item.pDate1.strftime('%Y-%m-%d'),
+                item.pDate2.strftime('%Y-%m-%d'),
+                '<a href="/edit?key=%s">Edit</a><br/><a href="/edit/character?key=%s">Edit Characters</a>'%(item,item)
+                ])
+
+        jsondata = {
+            'aaData' : datalist
+        }
+
+        self.response.out.write(json.dumps(jsondata))
+
+class ShowCompanyComplete(webapp.RequestHandler):
+    def get(self):
+        comps = Company.all()
+        comps.order('__key__')
+        datalist = []
+        i=1
+        for comp in comps:
+            datalist.append([
+                i,
+                comp.Name
+                ])
+            i+=1
+
+        jsondata = {
+            'aaData' : datalist
+        }
+
+        self.response.out.write(json.dumps(jsondata))
 
 class ShowCenter(webapp.RequestHandler):
     def get(self):
@@ -38,7 +176,7 @@ class ShowCenter(webapp.RequestHandler):
                 'chara' : {
                     'serve_url' : '/show/chara', 
                     'columns' : '5', 
-                    'headers': ['Image', 'CID', 'Name', 'Seiyuu', 'SID']
+                    'headers': ['Image', 'CID', 'Name', 'Romaji', 'Seiyuu', 'SID', 'Action']
                     },
                 'seiyuu' : {
                     'serve_url' : '/show/seiyuu', 
@@ -61,96 +199,82 @@ class ShowCenter(webapp.RequestHandler):
             path = os.path.join(os.path.dirname(__file__), './/template//show.html')
             self.response.out.write(template.render(path,template_values))
 
-class ShowCharacters(webapp.RequestHandler):
+class ShowSearch(webapp.RequestHandler):
     def get(self):
+        type_map = {
+            'Character': {
+                'headers':['Games','GamesCount','Name','isMain','sid','snum'],
+                'before':[],
+                'after':[]
+            },
+            'Game': {
+                'headers':['Characters','Company','Days','Finish_Date','Genre','Name','Release_Date','Romajis','Seiyuus','Start_Date','Time'],
+                'before':[],
+                'after':[]
+            },
+            'Seiyuu': {
+                'headers':['Name','cid','sid','Romaji','Seiyuu','SeiyuuMain'],
+                'before':[],
+                'after':[]
+            },
+        }
+        sortdir_map = {
+            'asc' : search.SortExpression.ASCENDING,
+            'desc': search.SortExpression.DESCENDING
+        }
+        # if len(colinfo['headers'])!=len(colinfo['before']) || len(colinfo['headers'])!=len(colinfo['after']):
+        #     return
+        index_name = self.request.get('type','')
+        keyword = self.request.get('sSearch','')
+        sEcho = self.request.get('sEcho',0)
+        sort = self.request.get('iSortingCols','0')
+        sortdir = self.request.get('sSortDir','asc')
         charas = Character.all()
+        iTotalRecords = charas.count()
+        iDisplayRecords = 0
+        start = self.request.get('iDisplayStart','0')
+        count = self.request.get('iDisplayLength','10')
 
-        charas.order('cid')
+        colinfo = type_map[index_name]
+        try:
+            index = search.Index(index_name)
+            options = search.QueryOptions(
+                limit=int(count), 
+                offset=int(start),
+                sort_options=search.SortOptions(expressions=[search.SortExpression(
+                        expression=colinfo['headers'][int(sort)],
+                        direction=sortdir_map[sortdir]
+                    )])
+                )
+            query = search.Query(keyword, options)
+            results = index.search(query)
+            iDisplayRecords = results.number_found
+        except search.Error:
+            logging.exception('Search %s Failed: %s'%(index_name, keyword))
 
         datalist = []
-        for chara in charas:
-            datalist.append([
-                '<img src="/data?key=%s" width=50 height=50/>' % chara.image.key(),
-                chara.cid,
-                chara.Name,
-                chara.getSeiyuuName(),
-                chara.sid
-                ])
+        logging.info('%s : %d'%(keyword, results.number_found))
+        for res in results:
+            datalist.append([unicode(res.fields[x].value) for x in xrange(0,len(res.fields))])
 
         #----end------------------------------------------------------------------------------------------
         jsondata = {
+            'sEcho' : int(sEcho),
+            'iTotalRecords' : iTotalRecords,
+            'iTotalDisplayRecords' : iDisplayRecords,
             'aaData' : datalist
         }
-        self.response.out.write(json.dumps(jsondata))
-
-
-class ShowSeiyuu(webapp.RequestHandler):
-    def get(self):
-        seiyuus = Seiyuu.all()
-        seiyuus.order('sid')
-        datalist = []
-        for item in seiyuus:
-            datalist.append([
-                item.sid,
-                item.Name,
-                str(item.isMain),
-                '<a href="http://beautyplanets.web.fc2.com/VADB/actor/c_%04d.html">%d</a>'%(item.snum,item.snum)
-                ])
-
-        jsondata = {
-            'aaData' : datalist
-        }
-
-        self.response.out.write(json.dumps(jsondata))
-
-class ShowGame(webapp.RequestHandler):
-    def get(self):
-        games = game.all()
-        games.order('rDate')
-        datalist = []
-        for item in games:
-            datalist.append([
-                '<img src="/data?key=%s" width=50 height=50/>'%item.Icon.key(),
-                item.Name,
-                db.get(item.Company).Name,
-                item.rDate.strftime('%Y-%m-%d'),
-                item.pDate1.strftime('%Y-%m-%d'),
-                item.pDate2.strftime('%Y-%m-%d'),
-                '<a href="/edit?key=%s">Edit</a><br/><a href="/edit/character?key=%s">Edit Characters</a>'%(item,item)
-                ])
-
-        jsondata = {
-            'aaData' : datalist
-        }
-
-        self.response.out.write(json.dumps(jsondata))
-
-class ShowCompany(webapp.RequestHandler):
-    def get(self):
-        comps = Company.all()
-        comps.order('__key__')
-        datalist = []
-        i=1
-        for comp in comps:
-            datalist.append([
-                i,
-                comp.Name
-                ])
-            i+=1
-
-        jsondata = {
-            'aaData' : datalist
-        }
-
         self.response.out.write(json.dumps(jsondata))
 
 def main():
     application = webapp.WSGIApplication([
         (r'/show', ShowCenter),
-        (r'/show/chara', ShowCharacters),
-        (r'/show/seiyuu', ShowSeiyuu),
-        (r'/show/game', ShowGame),
-        (r'/show/comp', ShowCompany),
+        (r'/show/complete', ShowCenterComplete),
+        (r'/show/complete/chara', ShowCharactersComplete),
+        (r'/show/complete/seiyuu', ShowSeiyuuComplete),
+        (r'/show/complete/game', ShowGameComplete),
+        (r'/show/complete/comp', ShowCompanyComplete),
+        (r'/show/search', ShowSearch),
         (r'/show.*', ShowCenter)
         ],debug=True)
     run_wsgi_app(application)
